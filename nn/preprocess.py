5f91e165c301f490698cee95fad6ed2e5361496d
s-miramontes <silvsilva@berkeley.edu>
2022-03-09 10:32:37 -0800

    edits

--- nn/preprocess.py
+++ nn/preprocess.py
@@ -29,7 +29,28 @@ def one_hot_encode_seqs(seq_arr: List[str]) -> ArrayLike:
                 G -> [0, 0, 0, 1]
             Then, AGA -> [1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0]
     """
-    pass
+    dict_seqs = {'A':[1, 0, 0, 0],
+                 'T':[0, 1, 0, 0],
+                 'C':[0, 0, 1, 0],
+                 'G':[0, 0, 0, 1]}
+
+    # for saving the 1-hot encoded sequence
+    encodings = []
+
+    # iterating through sequences in seq_arr
+    for seq in seq_arr:
+
+        # for storing encodings 
+        oh_coded = []
+        for letter in seq:
+            # given 'letter', add the values in dict_seqs
+            oh_coded += dict_seqs[letter]
+        
+        # add the one hot encoded sequence to encoding list
+        encodings += [oh_coded]
+    
+    # since asked for array, convert
+    return np.array(encodings)
 
 
 def sample_seqs(
@@ -51,4 +72,28 @@ def sample_seqs(
         sampled_labels: List[bool]
             List of labels for the sampled sequences
     """
-    pass
+    # total num of labels
+    n_labels = len(labels)
+    # since we want equal amount of class representation assume 50/50 split
+    size = np.ceil(n_labels/2)
+
+    # zip labels and sequences
+    seq_labels = list(zip(seqs, labels))
+
+    # get pos and neg labels
+    pos_labels = [el[0] for el in seq_labels if el[1] == True]
+    neg_labels = [el[0] for el in seq_labels if el[0] == False]
+
+    # resample from uniform distribution 
+    pos_sample = np.random.choice(pos_labels, size=size)
+    pos_sample = [(el, True) for el in pos_sample]
+    neg_sample = np.random.choice(neg_labels, size=size)
+    neg_sample = [(el, False) for el in neg_sample]
+    all_ = pos_sample + neg_sample
+    np.random.shuffle(all_)
+
+    sampled_sequences = [j[0] for j in all_]
+    sampled_labels = [j[1] for j in all_]
+
+    return sampled_sequences, sampled_labels
+
